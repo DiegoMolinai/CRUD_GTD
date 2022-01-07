a90ef42fe961c6651506ab334823822d690f22fa
Diego <diego.molina@gtdcloud.onmicrosoft.com>
2022-01-07 14:44:47 +0000

    Cambios De Prueba

--- Database/database.sql
+++ Database/database.sql
@@ -2,6 +2,8 @@ CREATE DATABASE ng_tareas_db;
 
 USE ng_tareas_db;
 
+DROP TABLE tareas;
+
 CREATE TABLE tareas(
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(180),
--- Server/build/database.js
+++ Server/build/database.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: '52.147.166.93',
+    port: '3306',
     user: 'root',
     database: 'ng_tareas_db',
-    password: 'admin',
+    password: 'diegoignacio',
     waitForConnections: true,
     queueLimit: 0
 });
