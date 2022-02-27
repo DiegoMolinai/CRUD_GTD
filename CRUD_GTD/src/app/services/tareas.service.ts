fc520871820f925ac013efd15ebbabb0f1cbe23f
Ubuntu <azureuser@MaquinaKubernetes.n5wismzoywwuncsrq5ykqmvwsc.cx.internal.cloudapp.net>
2022-02-27 22:03:43 +0000

    pasar lo de la maquina virtual

--- CRUD_GTD/src/app/services/tareas.service.ts
+++ CRUD_GTD/src/app/services/tareas.service.ts
@@ -12,24 +12,24 @@ export class TareasService {
   constructor(private http: HttpClient) { }
 
   getTareas(){
-    return this.http.get('http://20.114.144.160:3000/api/tareas/');
+    return this.http.get('http://20.94.18.84:3000/api/tareas/');
   }
 
   getTarea(id:string){
-    return this.http.get('http://20.114.144.160:3000/api/tareas/'+ id);
+    return this.http.get('http://20.94.18.84:3000/api/tareas/'+ id);
   }
 
   saveTarea(tarea:Tarea){
-    return this.http.post('http://20.114.144.160:3000/api/tareas',tarea);
+    return this.http.post('http://20.94.18.84:3000/api/tareas',tarea);
   }
 
   deleteTarea(id:string){
-    return this.http.delete('http://20.114.144.160:3000/api/tareas/' + id);
+    return this.http.delete('http://20.94.18.84:3000/api/tareas/' + id);
   }
 
   updateTarea(id: string,updatedTarea: Tarea):Observable<Tarea> {
     console.log(updatedTarea);
     console.log(id);
-    return this.http.put('http://20.114.144.160:3000/api/tareas/'+id,updatedTarea);
+    return this.http.put('http://20.94.18.84:3000/api/tareas/'+id,updatedTarea);
   }
 }
